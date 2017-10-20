414c7bb125b6150248341d98345a2e136699fa6c
vilara <marcelovilara@gmail.com>
2017-10-20 14:17:55 -0200

    Menu sucessufully

---
+++ vilarashopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,15 @@
+$(function (){
+	switch (menu) {
+	case 'About Us':
+		$('#about').addClass('active')
+		break;
+
+	case 'Contact':
+		$('#contact').addClass('active')
+		break;
+		
+	default:
+		$('#home').addClass('active')
+		break;
+	}
+});
