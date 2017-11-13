5c22002953b5f209b82dba4f23ef1b62056496f2
vilara <marcelovilara@gmail.com>
2017-11-13 16:58:22 -0200

    security from jdbc configured

--- vilarashoppingbackend/src/main/java/br/com/vilara/vilarashopping/config/HibernateConfig.java
+++ vilarashoppingbackend/src/main/java/br/com/vilara/vilarashopping/config/HibernateConfig.java
@@ -26,7 +26,7 @@ public class HibernateConfig {
 	private final static String DATABASE_PASSWORD = "";
 	
 	// datasource bean will be available 
-	@Bean
+	@Bean("dataSource") // este nome é usado na configuracao do spring-security.xml
 	public DataSource getDataSource() {
 		
 		BasicDataSource dataSource = new BasicDataSource();
